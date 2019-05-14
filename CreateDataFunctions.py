8f2ae78458eff006af01311a2b0167cc61864392
Erik Forsberg <erikforsberg95@hotmail.com>
2019-05-14 13:41:25 +0200

    Create CreateDataFunctions.py

---
+++ CreateDataFunctions.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import random
+import math
+from sklearn.utils import shuffle
+
+def CreateData1(nrDataPoints,radius,center,noise,classNr,nrFeatures,grid):
+   
+    return data
