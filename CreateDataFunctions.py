80d2b238317cf59adefd2773a97f89dcf38c9699
NiklasOPF <nikfor@student.chalmers.se>
2019-05-15 07:34:19 +0200

    added function for generating standart normal data

--- CreateDataFunctions.py
+++ CreateDataFunctions.py
@@ -1,10 +1,26 @@
-import matplotlib.pyplot as plt
+ximport matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import random
 import math
-from sklearn.utils import shuffle
+#from sklearn.utils import shuffle
+
+def normal_data(nrObservations, nrFeatures):
+    """
+    Generates a standard normally distibuted dataset with "nrFeatures" dimensions and "nrObservations" observations
+    
+    :param int nrFeatures: Number of dimensions in the dataset
+    :param int nrDataPoints: Number of observations in the dataset
+    :return data: numpy array with data  (one observation per row)
+    """
+    data = np.random.normal(loc=0.0, scale=1.0, size=[nrObservations, nrFeatures])
+    
+    return data
+
+
+
+
 
 def CreateData1(nrDataPoints = 1000, nrFeatures = 30):
-   
+    
     return data
